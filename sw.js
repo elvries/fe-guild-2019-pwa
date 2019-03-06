ad9282e27b5f2adc95825803e34fdf46db87cfaf
Elwin de Vries <elwin.de.vries@ing.com>
2019-03-06 15:00:44 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', event => {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-    event.waitUntil(self.skipWaiting());
-});
+// self.addEventListener('install', event => {
+//     console.log('[Service Worker] Installing Service Worker ...', event);
+//     event.waitUntil(self.skipWaiting());
+// });
 
 self.addEventListener('activate', event => {
     console.log('[Service Worker] Activating Service Worker ...', event);
